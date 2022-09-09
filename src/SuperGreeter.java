0b4df2fe6551f1f2b7b027f37ffd276e8468de36
SimonBD1 <simo20k3@stud.kea.dk>
2022-09-09 14:29:30 +0200

    Lavet metoder og scanner til navn

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
+        Scanner scanner = new Scanner(System.in);
+
+
+
     public void start(){
         System.out.println("det virker!");
+
+    }
+    public void greet (){
+        System.out.println("Hvad hedder du?");
+        String navn =scanner.nextLine();
+        System.out.println("hej "+navn);
+
+
     }
     public static void main (String [] args) {
-        SuperGreeter sg = new SuperGreeter();
-        sg.start();
+    SuperGreeter sg= new SuperGreeter();
+    sg.start();
+    sg.greet();
 
     }
 }
